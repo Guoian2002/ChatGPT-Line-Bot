08f09f37acca41df69a663dbc8fad8a0c683a485
Guoian2002 <q0905597749@gmail.com>
2023-05-25 22:43:25 +0800

    test11

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ def handle_text_message(event):
                 chat=False
             elif text=='我想要查詢心理醫療機構':
                 msg=TextSendMessage(
-                    text="",
+                    text="你好",
                     quick_reply=QuickReply(
                     items=[
                         QuickReplyButton(
