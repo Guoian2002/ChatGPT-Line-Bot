007ab0accc6b3c3bb25cd69c8bd3b592117f3735
Guoian2002 <q0905597749@gmail.com>
2023-06-05 22:02:36 +0800

    test2222

--- main.py
+++ main.py
@@ -138,7 +138,10 @@ def handle_text_message(event):
                 ]                      
             )
         )
-            
+              
+
+
+              
 
         elif text == '總結':
             conversation = user_messages[user_id] + assistant_messages[user_id]
